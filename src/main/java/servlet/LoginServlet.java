127477b41580dd6bfaf721322a730f5c4a27d7c5
Atheek Azmi <138520822+AtheekAzmi@users.noreply.github.com>
2025-08-14 14:02:19 +0530

    Update LoginServlet.java

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -3,16 +3,14 @@ package servlet;
 import dao.UserDAO;
 import model.User;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import jakarta.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.annotation.WebServlet; // Use only this if on Jakarta
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/login") // Optional if also mapped in web.xml
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     private UserDAO userDAO;
@@ -24,38 +22,17 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws IOException {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-//        System.out.println("[DEBUG] Received credentials: " + username + " / " + password);
-
-        // User loginUser = new User(username, password);
-
-        // try {
-        //     if (userDAO.validate(loginUser)) {
-        //         HttpSession session = request.getSession();
-        //         session.setAttribute("username", username);
-        //         session.setAttribute("password", password);
-        //         response.sendRedirect("dashboard.jsp");
-        //     } else {
-        //         response.sendRedirect("index.jsp?error=true");
-        //     }
-        // } catch (ClassNotFoundException e) {
-        //     e.printStackTrace();
-        // }
-        // use the DAO to validate user
         if(userDAO.validateUser(username, password)){
             HttpSession session = request.getSession();
-            session.setAttribute("user", username);
-            // redirect to dashboard controller or JSP
+            session.setAttribute("username", username);
             response.sendRedirect("dashboard.jsp");
         } else {
-            // Pass error back to view
-            resp.sendRedirect("index.jsp?error=true");
+            response.sendRedirect("index.jsp?error=true");
         }
-                
-
     }
 }
